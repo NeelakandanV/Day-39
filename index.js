2fa3c9d899b3e04b0da999fde5aecb406837efa8
NeelakandanV <hari0709venkatesan@gmail.com>
2024-04-27 14:00:18 +0530

    sixth commit

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ app.get("/gettime",(req,res)=>{
         if(err){
             console.log(err)
         }else{
-           res.send(`The text files available in the timestamp folder are, ${data}`)
+           res.send(`The text files available in the timestamp folder are, \n${data}`)
         }
     })
 })
